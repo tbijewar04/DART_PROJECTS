3e9e1d0a907e02c566c7a01f08c82aecd516c8a9
tbijewar04 <tejasbijewar5391@gmail.com>
2024-09-30 20:30:49 +0530

    Update Quiz_app.dart.

--- Class_Apps/StateFullWidget/Quiz_app.dart.dart
+++ Class_Apps/StateFullWidget/Quiz_app.dart.dart
@@ -24,164 +24,314 @@ class QuizApp extends StatefulWidget {
 class _QuizAppState extends State {
   List<Map> allQuestions = [
     {
-      "question": "Who is the founder of Microsoft?",
-      "options": ["Steve Jobs", "Bill Gates", "Lary Page", "Elon musk"],
+      "question": "Which keyword is used to declare a constant value in Dart?",
+      "options": ["final", "const", "static", "let"],
       "correctAnswer": 1
     },
     {
-      "question": "Who is the founder of Google?",
-      "options": ["Steve Jobs", "Bill Gates", "Lary Page", "Elon musk"],
+      "question":
+          "Which data type is used to represent true or false values in Dart?",
+      "options": ["int", "boolean", "bool", "double"],
       "correctAnswer": 2
     },
     {
-      "question": "Who is the founder of SpaceX?",
-      "options": ["Steve Jobs", "Bill Gates", "Lary Page", "Elon musk"],
+      "question": "What is the return type of the main() function in Dart?",
+      "options": ["int", "double", "string", "void"],
       "correctAnswer": 3
     },
     {
-      "question": "Who is the founder of Apple?",
-      "options": ["Steve Jobs", "Bill Gates", "Lary Page", "Elon musk"],
+      "question": "How can a class include a mixin in Dart?",
+      "options": ["with", "extends", "implements", "inherits"],
       "correctAnswer": 0
     },
     {
-      "question": "Who is the founder of Meta?",
-      "options": ["Steve Jobs", "Mark Zuckerberg", "Lary Page", "Elonmusk"],
+      "question": "Which keyword is used to inherit a class in Dart?",
+      "options": ["inherits", "extends", "implements", "with"],
       "correctAnswer": 1
     },
   ];
+
   int currentQuestionIndex = 0;
+  int selectedAnswerIndex = -1;
+  int currentscore = 0;
+
+  WidgetStateProperty<Color?> checkAnswer(int answerIndex) {
+    if (selectedAnswerIndex != -1) {
+      if (answerIndex == allQuestions[currentQuestionIndex]["correctAnswer"]) {
+        if (selectedAnswerIndex ==
+            allQuestions[currentQuestionIndex]["correctAnswer"]) {
+          currentscore++;
+        }
+        return const WidgetStatePropertyAll(Colors.green);
+      } else if (selectedAnswerIndex == answerIndex) {
+        return const WidgetStatePropertyAll(Colors.red);
+      } else {
+        return const WidgetStatePropertyAll(null);
+      }
+    } else {
+      return const WidgetStatePropertyAll(null);
+    }
+  }
+
+  bool questionPage = true;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Quiz App",
-          style: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.w900,
-            color: Colors.orange,
+    return isQuestionScreen();
+  }
+
+  Scaffold isQuestionScreen() {
+    if (questionPage == true) {
+      return Scaffold(
+        backgroundColor: Color.fromRGBO(250, 241, 178, 1),
+        appBar: AppBar(
+          title: const Text(
+            "Quiz App",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w900,
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+
+            // QUESTION NUMBER
+            Row(
+              children: [
+                const SizedBox(
+                  width: 150,
+                ),
+                Text(
+                  "Question : ${currentQuestionIndex + 1}/${allQuestions.length}",
+                  style: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+
+            //QUESTION
+            SizedBox(
+              width: 400,
+              height: 80,
+              child: Text(
+                textAlign: TextAlign.left,
+                allQuestions[currentQuestionIndex]["question"],
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+
+            //OPTION 1
+            SizedBox(
+              height: 50,
+              width: 350,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: checkAnswer(0),
+                ),
+                onPressed: () {
+                  if (selectedAnswerIndex == -1) {
+                    selectedAnswerIndex = 0;
+                    setState(() {});
+                  }
+                },
+                child: Text(
+                  "A." " ${allQuestions[currentQuestionIndex]['options'][0]}",
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+
+            //OPTION 2
+            SizedBox(
+              height: 50,
+              width: 350,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: checkAnswer(1),
+                ),
+                onPressed: () {
+                  if (selectedAnswerIndex == -1) {
+                    selectedAnswerIndex = 1;
+                    setState(() {});
+                  }
+                },
+                child: Text(
+                  "B." " ${allQuestions[currentQuestionIndex]['options'][1]}",
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+
+            // OPTION 3
+            SizedBox(
+              height: 50,
+              width: 350,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: checkAnswer(2),
+                ),
+                onPressed: () {
+                  if (selectedAnswerIndex == -1) {
+                    selectedAnswerIndex = 2;
+                    setState(() {});
+                  }
+                },
+                child: Text(
+                  "C." " ${allQuestions[currentQuestionIndex]['options'][2]}",
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+
+            // OPTION 4
+            SizedBox(
+              height: 50,
+              width: 350,
+              child: ElevatedButton(
+                onPressed: () {
+                  if (selectedAnswerIndex == -1) {
+                    selectedAnswerIndex = 3;
+                    setState(() {});
+                  }
+                },
+                style: ButtonStyle(
+                  backgroundColor: checkAnswer(3),
+                ),
+                child: Text(
+                  "D." " ${allQuestions[currentQuestionIndex]['options'][3]}",
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (selectedAnswerIndex != -1) {
+              // THIS CHECK THE CONDITION AND SHOWS THE RESULT SCREEN
+              if (currentQuestionIndex < allQuestions.length - 1) {
+                currentQuestionIndex++;
+              } else {
+                questionPage = false;
+              }
+              selectedAnswerIndex = -1;
+              setState(() {});
+            }
+          },
+          backgroundColor: Colors.blue,
+          child: const Icon(
+            Icons.forward,
+            color: Colors.white,
           ),
         ),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 30,
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Quiz Result",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.w900,
+              color: Colors.white,
+            ),
           ),
-          Row(
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(
-                width: 380,
+              Image.network(
+                "https://png.pngtree.com/png-clipart/20220117/original/pngtree-trophy-gold-cup-award-winning-decorative-elements-png-image_7121786.png",
+                height: 300,
               ),
+              const SizedBox(height: 20),
               Text(
-                "Question : ${currentQuestionIndex + 1}/${allQuestions.length}",
+                "Score: $currentscore/${allQuestions.length}",
                 style: const TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w700,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 7, 72, 126)),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              SizedBox(
+                height: 50,
+                width: 100,
+                child: ElevatedButton(
+                  onPressed: () {
+                    currentQuestionIndex = 0;
+                    selectedAnswerIndex = -1;
+                    currentscore = 0;
+                    questionPage = true;
+                    setState(() {});
+                  },
+                  child: const Text(
+                    "Reset",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
-          const SizedBox(
-            height: 50,
-          ),
-          SizedBox(
-            width: 380,
-            height: 50,
-            child: Text(
-              allQuestions[currentQuestionIndex]["question"],
-              style: const TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.w600,
-                color: Colors.purple,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          SizedBox(
-            height: 50,
-            width: 350,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                allQuestions[currentQuestionIndex]['options'][0],
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          SizedBox(
-            height: 50,
-            width: 350,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                allQuestions[currentQuestionIndex]['options'][1],
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          SizedBox(
-            height: 50,
-            width: 350,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                allQuestions[currentQuestionIndex]['options'][2],
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          SizedBox(
-            height: 50,
-            width: 350,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                allQuestions[currentQuestionIndex]['options'][3],
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (currentQuestionIndex < allQuestions.length - 1) {
-            currentQuestionIndex++;
-            setState(() {});
-          }
-        },
-        backgroundColor: Colors.blue,
-        child: const Icon(
-          Icons.forward,
-          color: Colors.orange,
         ),
-      ),
-    );
+      );
+    }
   }
 }
+
